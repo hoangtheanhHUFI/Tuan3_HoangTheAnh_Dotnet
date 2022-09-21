e9778d6c5efd912ed173aed14cb962a6f42fb87f
Hoang The Anh <2001202008@hufi.edu.vn>
2022-09-21 16:08:26 +0700

    UPdate

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,17 +5,19 @@ using Tuan3_HoangTheAnh.Models;
 
 namespace Tuan3_HoangTheAnh.Controllers
 {
+    
     public class HomeController : Controller
     {
+        static List<modelSach> ds = new List<modelSach>();
         public IActionResult Index()
         {
-            modelSach a = new modelSach("The Anh",30000, "/lib/image/img.jpg");
-            modelSach b = new modelSach("Cuong thuan", 50000, "/lib/image/img.jpg");
-            modelSach c = new modelSach("Chi tam", 35000, "/lib/image/img.jpg");
-            modelSach d = new modelSach("The Anh", 63000, "/lib/image/img.jpg");
-            modelSach e = new modelSach("The Anh", 30000, "/lib/image/img.jpg");
-            modelSach f = new modelSach("The Anh", 30000, "/lib/image/img.jpg");
-            modelSach g = new modelSach("The Anh", 30000, "/lib/image/img.jpg");
+            modelSach a = new modelSach("1","The Anh",30000, "/lib/image/img.jpg");
+            modelSach b = new modelSach("1", "Cuong thuan", 50000, "/lib/image/img.jpg");
+            modelSach c = new modelSach("1", "Chi tam", 35000, "/lib/image/img.jpg");
+            modelSach d = new modelSach("1", "The Anh 1", 63000, "/lib/image/img.jpg");
+            modelSach e = new modelSach("1", "The Anh 2", 30000, "/lib/image/img.jpg");
+            modelSach f = new modelSach("1", "The Anh 3", 30000, "/lib/image/img.jpg");
+            modelSach g = new modelSach("1 ", "The Anh 4", 34000, "/lib/image/img.jpg");
 
 
             List<modelSach> ds = new List<modelSach>();
@@ -25,6 +27,11 @@ namespace Tuan3_HoangTheAnh.Controllers
 
 
             return View(ds);
+        }
+        public ActionResult ChiTiet(string Id)
+        {
+            modelSach a = ds.FirstOrDefault(x => x.id.Equals(Id));
+            return View(a);
         }
     }
 }
--- Models/modelSach.cs
+++ Models/modelSach.cs
@@ -2,18 +2,21 @@
 {
     public class modelSach
     {
+        public String id { get; set; }
         public string tenSach { get; set; }
         public double dongiaSach { get; set; }
         public string hinhSach { get; set; }
         public modelSach()
         {
+            id = "0";
             tenSach = "Sach A";
             dongiaSach = 0;
             hinhSach = "/lib/image/img.jpg";
 
         }
-        public modelSach(string tenSach, double dongiaSach, string hinhSach)
+        public modelSach(string id ,string tenSach, double dongiaSach, string hinhSach)
         {
+            this.id = id;
             this.tenSach = tenSach;
             this.dongiaSach = dongiaSach;
             this.hinhSach = hinhSach;
