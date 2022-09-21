8c2516149f3d468a00beb9a20d673e4d7f47e970
Hoang The Anh <2001202008@hufi.edu.vn>
2022-09-21 15:11:11 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Tuan3_HoangTheAnh.Models;
+
+
+namespace Tuan3_HoangTheAnh.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            modelSach a = new modelSach("The Anh",30000, "/lib/image/img.jpg");
+            modelSach b = new modelSach("Cuong thuan", 50000, "/lib/image/img.jpg");
+            modelSach c = new modelSach("Chi tam", 35000, "/lib/image/img.jpg");
+            modelSach d = new modelSach("The Anh", 63000, "/lib/image/img.jpg");
+            modelSach e = new modelSach("The Anh", 30000, "/lib/image/img.jpg");
+            modelSach f = new modelSach("The Anh", 30000, "/lib/image/img.jpg");
+            modelSach g = new modelSach("The Anh", 30000, "/lib/image/img.jpg");
+
+
+            List<modelSach> ds = new List<modelSach>();
+
+            ds.Add(a); ds.Add(b); ds.Add(c); ds.Add(d); ds.Add(e); ds.Add(f); ds.Add(g);
+
+
+
+            return View(ds);
+        }
+    }
+}
---
+++ Models/modelSach.cs
@@ -0,0 +1,22 @@
+﻿namespace Tuan3_HoangTheAnh.Models
+{
+    public class modelSach
+    {
+        public string tenSach { get; set; }
+        public double dongiaSach { get; set; }
+        public string hinhSach { get; set; }
+        public modelSach()
+        {
+            tenSach = "Sach A";
+            dongiaSach = 0;
+            hinhSach = "/lib/image/img.jpg";
+
+        }
+        public modelSach(string tenSach, double dongiaSach, string hinhSach)
+        {
+            this.tenSach = tenSach;
+            this.dongiaSach = dongiaSach;
+            this.hinhSach = hinhSach;
+        }
+    }
+}
